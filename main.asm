8bf423a19dcc01dca005aa486e319d1b0c73c014
Frederico Maia Estrella <91262593+FredMaia@users.noreply.github.com>
2024-11-28 13:05:57 -0300

    feat: add program template

---
+++ main.asm
@@ -0,0 +1,16 @@
+# Title: Questão x
+# Author: Seu nome
+# Description: Enunciado
+# Input: Ex.: Número em ponto flutuante que representa a parcela
+# Output: saída
+
+################# Data segment #####################
+.data
+...
+################# Code segment #####################
+.text
+.globl main
+main:
+...
+li $v0, 10
+syscall
